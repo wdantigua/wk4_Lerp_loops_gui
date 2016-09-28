ae81236a4acee6720faea627af533de7ab491db0
Wilson Diaz <diazw291@newschool.edu>
2016-09-28 18:27:21 -0400

    learning leap and look at code

---
+++ linear interpolation/Assets/cylFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cylFollow : MonoBehaviour {
+
+
+	public Transform thingToFollow;
+	public float sp = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (thingToFollow);
+		transform.position += transform.forward * Time.deltaTime * sp;
+	}
+}
---
+++ linear interpolation/Assets/lerpExtreme.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lerpExtreme : MonoBehaviour {
+
+	public Transform pointA;
+	public Transform pointB;
+
+
+	public float per =0;
+	public float dir =-1;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+		{
+	
+		transform.position = Vector3.Lerp (pointA.position, pointB.position, per);
+		transform.rotation = Quaternion.Slerp (pointA.rotation, pointB.rotation, per);
+		per = per + Time.deltaTime * dir;
+
+		if (per > 1) 
+		{
+			dir = -1;
+		}
+
+		if (per < 0) 
+		{
+			dir = 1;
+		}
+	}
+}
